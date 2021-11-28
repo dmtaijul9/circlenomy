f61d3915736ebc5b676d16e3025e7b90323163e7
Taijul Islam <taijulislam.st9@gmail.com>
2021-11-28 13:42:41 +0600

    Adding: doing responsive for sign up page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -11,12 +11,18 @@ function SignUp() {
     <section className="sign-up-section">
       <Container>
         <Row justify="space-between" gutter={16} wrap>
-          <Col span={8} className="vactor-div">
+          <Col xl={8} lg={{ span: 4, push: 1 }} className="vactor-div">
             <div>
               <img src={signUpVictor} alt="victor" className="sign-up-victor" />
             </div>
           </Col>
-          <Col span={8} offset={8}>
+          <Col
+            xl={{ span: 8, offset: 8, pull: 0 }}
+            lg={{ span: 8, pull: 2 }}
+            md={{ span: 16, offset: 7 }}
+            sm={{ span: 16, offset: 5 }}
+            xs={{ span: 20, offset: 2 }}
+          >
             <SignUpForm />
           </Col>
         </Row>
